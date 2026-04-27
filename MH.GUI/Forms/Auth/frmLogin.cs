7a9330a4cb81f8a8cc904ef3604d5d0d5854ce5c
MinDuck <nguyenngocminhduc2005@gmail.com>
2026-04-27 19:54:25 +0700

    chuc nang dang nhap

--- MH.GUI/Forms/Auth/frmLogin.cs
+++ MH.GUI/Forms/Auth/frmLogin.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MH.BLL; // Gọi thư viện tầng BLL
 
 namespace MH.GUI.Forms.Auth
 {
@@ -17,9 +11,26 @@ namespace MH.GUI.Forms.Auth
             InitializeComponent();
         }
 
-        private void lblWelcome_Click(object sender, EventArgs e)
+        private void btnLogin_Click(object sender, EventArgs e)
         {
+            string user = txtUsername.Text;
+            string pass = txtPassword.Text;
 
+            TaiKhoanBLL bll = new TaiKhoanBLL();
+            string ketQua = bll.DangNhap(user, pass);
+
+            if (ketQua == "OK")
+            {
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MH.GUI.Forms.Main.Trangchu fTrangChu = new MH.GUI.Forms.Main.Trangchu();
+                this.Hide();
+                fTrangChu.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show(ketQua, "Lỗi đăng nhập", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
